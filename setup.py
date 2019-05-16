a463550e02e13057ae2eed9c93bf09967b76804f
Ian Nesbitt <ian.nesbitt@gmail.com>
2019-05-15 23:12:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="gpx2dzg",
+    version="0.0.0",
+    author="Ian Nesbitt",
+    author_email="ian.nesbitt@gmail.com",
+    license='GPLv3',
+    description="convert GPX files to GSSI's proprietary DZG format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iannesbitt/gpx2dzg",
+    packages=setuptools.find_packages(),
+    install_requires=['geomag', 'pynmea2'],
+    entry_points='''
+        [console_scripts]
+        gpx2dzg=gpx2dzg.gpx2dzg:main
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Natural Language :: English",
+        "Development Status :: 4 - Beta",
+    ],
+)
